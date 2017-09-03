9847958f86ff145dced2a33a032196bc1c407ed3
Aryan <alikhaniaryan@live.com>
2017-09-03 12:55:10 +0430

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='nodel',
+	version='0.1.0',
+	packages=['nodel'],
+	url='https://github.com/ary4n/nodel',
+	license='MIT',
+	author='aryan',
+	author_email='alikhaniaryan@live.com',
+	description='django project manager',
+	download_url='https://github.com/ary4n/nodel/archive/0.1.0.tar.gz'
+)
